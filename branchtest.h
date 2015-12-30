523e918d771c55dc8f45694775b41e374891d4a9
Chickenpup <christophergshea@gmail.com>
2015-12-30 11:01:57 -0500

    Added branchtest class

---
+++ branchtest.h
@@ -0,0 +1,13 @@
+//
+//  branchtest.h
+//  GitTest
+//
+//  Created by Chris on 12/30/15.
+//  Copyright © 2015 Prince Fungus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface branchtest : NSObject
+
+@end
---
+++ branchtest.m
@@ -0,0 +1,13 @@
+//
+//  branchtest.m
+//  GitTest
+//
+//  Created by Chris on 12/30/15.
+//  Copyright © 2015 Prince Fungus. All rights reserved.
+//
+
+#import "branchtest.h"
+
+@implementation branchtest
+
+@end
